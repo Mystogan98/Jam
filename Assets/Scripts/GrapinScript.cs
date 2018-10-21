f691ed29a03b12ef3144cc7d589034a633adf6f7
Mystogan98 <shangheilis@hotmail.fr>
2018-10-21 04:08:43 +0200

    juste au cas ou

--- Assets/Scripts/GrapinScript.cs
+++ Assets/Scripts/GrapinScript.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class GrapinScript : MonoBehaviour {
 
 	public GameObject wagon;
-	public float speed, clampValue;
+	public float speed, clampValue, speedVertical;
+	public Sprite Closed;
 
-	private int direction = -1;
-	private float baseAngle, delta = -1f;
+	private int direction = 1;
+	private float baseAngle, delta = -1f, baseY;
 
 	private bool isLaunched = false;
+	private RaycastHit2D target;
+	private SpriteRenderer spr;
 
 	// Use this for initialization
 	void Start () {
 		baseAngle = transform.rotation.eulerAngles.z;
+		baseY = transform.position.y;
+		spr = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -26,7 +31,7 @@ public class GrapinScript : MonoBehaviour {
 				Vector2 parent = new Vector2(wagon.transform.position.x,wagon.transform.position.y);
 				Vector2 child = new Vector2(transform.position.x/* + parent.x*/, transform.position.y/* + parent.y*/);
 
-				RaycastHit2D hit2D = Physics2D.Raycast(child,
+				target = Physics2D.Raycast(child,
 												new Vector2(((child.x - parent.x) / 
 															(child.y - parent.y)) *
 															(child.y + delta),
@@ -35,7 +40,8 @@ public class GrapinScript : MonoBehaviour {
 															(child.y - parent.y)) *
 															(child.y + delta * 200),
 															child.y + delta * 200),Color.black,20);
-				Debug.DrawLine(child, hit2D.transform.position ,Color.red ,20);
+				Debug.DrawLine(child, target.transform.position ,Color.red ,20);
+				direction = -1;
 				isLaunched = true;
 			} else {
 				Debug.Log(transform.rotation.eulerAngles.z);
@@ -45,6 +51,14 @@ public class GrapinScript : MonoBehaviour {
 					direction = 1;
 				transform.RotateAround(wagon.transform.position,Vector3.forward,speed * Time.deltaTime * direction);
 			}
+		} else {
+			transform.Translate(new Vector3(0,direction * speedVertical * Time.deltaTime, 0),transform);
+			if(transform.position.y < target.transform.position.y)
+			{
+				spr.sprite = Closed;
+				direction = 1;
+			} else if (transform.position.y > baseY && spr.sprite == Closed)
+				direction = 0;
 		}
 	}
 }
